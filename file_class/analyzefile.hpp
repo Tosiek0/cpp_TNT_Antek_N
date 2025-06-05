1abf08464630a21ece6f4a7dd5d97c391c8b83fe
Tosiek0 <nieantek111@icloud.com>
2025-06-05 07:09:19 +0000

    file_class

---
+++ file_class/analyzefile.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "file.hpp"
+
+
+class AnalyzedFile : File
+{
+    // countWords
+    // countOccurrences(const std::string& word);
+    // countFileLines
+    // toUpperCase
+    // showStats
+};
