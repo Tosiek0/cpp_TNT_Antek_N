8af70c870f2384ee07ebddaa93cd7f1b9ff35162
Tosiek0 <nieantek111@icloud.com>
2025-06-10 09:00:11 +0000

    s

--- file_class/analyzefile.cpp
+++ file_class/analyzefile.cpp
@@ -1,88 +1,41 @@
 #include "analyzefile.hpp"
-#include <algorithm>
-#include <cctype>
-#include <iostream>
-#include <sstream>
 
-// Liczy wszystkie słowa we wszystkich liniach
 int AnalyzedFile::countWords()
 {
-    if (_lines.empty())
+    if (!_lines.empty())
     {
         return 0;
     }
-
-    int wordCount = 0;
-    for (const auto &line : _lines)
-    {
-        std::istringstream iss(line);
-        std::string word;
-        while (iss >> word)
-        {
-            ++wordCount;
-        }
-    }
-
-    return wordCount;
+    else ()
+    
 }
 
-// Liczy wystąpienia konkretnego słowa (ignoruje wielkość liter)
 int AnalyzedFile::countOccurrences(const std::string &word)
 {
-    if (_lines.empty())
-    {
-        return 0;
-    }
-
-    int count = 0;
-    std::string lowerWord = word;
-    std::transform(lowerWord.begin(), lowerWord.end(), lowerWord.begin(), ::tolower);
-
-    for (const auto &line : _lines)
-    {
-        std::istringstream iss(line);
-        std::string token;
-        while (iss >> token)
-        {
-            std::string loweredToken = token;
-            std::transform(loweredToken.begin(), loweredToken.end(), loweredToken.begin(), ::tolower);
-            if (loweredToken == lowerWord)
-            {
-                ++count;
-            }
-        }
-    }
-
-    return count;
+    return 0;
 }
 
-// Zwraca liczbę linii w pliku
 int AnalyzedFile::countFileLines()
 {
-    return _lines.size();
+    /*
+    if (!_lines.empty())
+    {
+        return _lines.size();
+    }
+    */
+    return 0;
 }
 
-// Zmienia wszystkie litery na wielkie
 void AnalyzedFile::toUpperCase()
 {
-    for (auto &line : _lines)
-    {
-        std::transform(line.begin(), line.end(), line.begin(), ::toupper);
-    }
+
 }
 
-// Zmienia wszystkie litery na małe
 void AnalyzedFile::toLowerCase()
 {
-    for (auto &line : _lines)
-    {
-        std::transform(line.begin(), line.end(), line.begin(), ::tolower);
-    }
+
 }
 
-// Wyświetla podstawowe statystyki
 void AnalyzedFile::showStats()
 {
-    std::cout << "Lines: " << countFileLines() << std::endl;
-    std::cout << "Words: " << countWords() << std::endl;
 }
